6db6ee000521f9e0814f8de69399f0b14be91a4d
Malcolm <echelon1089@gmail.com>
2019-11-05 19:43:26 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,12 @@
 module ArtistsHelper
 
-    def show_artist(song)
+    def display_artist(song)
         if !song.artist_name.nil?
             link_to song.artist_name, artist_path(song.artist)
+            #binding.pry
         else
             link_to 'Add Artist', edit_song_path(song)
+           #binding.pry
         end
     end
 end
